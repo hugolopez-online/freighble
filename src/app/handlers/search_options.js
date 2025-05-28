b9091d2222816fc9a09535ab229268c371412189
hugolopez-online <hugolopez.online@gmail.com>
2025-05-28 00:14:42 -0700

    feat(search): locations autocomplete (WIP)

---
+++ src/app/handlers/search_options.js
@@ -0,0 +1,27 @@
+export const search_options = [
+    {
+        search: "Victoria, BC",
+        city: "Victoria",
+        territory: "BC",
+    },
+    {
+        search: "Vancouver, BC",
+        city: "Vancouver",
+        territory: "BC",
+    },
+    {
+        search: "Vancouver, WA",
+        city: "Vancouver",
+        territory: "WA",
+    },
+    {
+        search: "Miami, FL",
+        city: "Miami",
+        territory: "FL",
+    },
+    {
+        search: "Miami, OH",
+        city: "Miami",
+        territory: "OH",
+    },
+];
