c3c224aa0d94d938227750eb9a2063cdc4437fe9
hugolopez-online <hugolopez.online@gmail.com>
2025-05-27 22:41:05 -0700

    chore(server): general updates and cleanup

--- server.cjs
+++ server.cjs
@@ -1,19 +1,3 @@
-// const http = require("http");
-// // TODO: replace module import with a fs readFile approach, so it reads from a .json file
-// const vendors = require("./vendors_module.cjs");
-
-// const server = http.createServer((req, res) => {
-//     console.log(`Server hit at "${req.url}"`);
-//     res.writeHead(200, { "Content-Type": "application/json" });
-//     return res.end(JSON.stringify(vendors));
-// });
-
-// const PORT = process.env.PORT || 8080;
-
-// server.listen(PORT, () => {
-//     return console.log(`Server ready and listening on port ${PORT}`);
-// });
-
 const express = require("express");
 const app = express();
 
