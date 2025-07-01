478e87151d35d3986699b5b91f9af2f1ddd09edf
hugolopez-online <hugolopez.online@gmail.com>
2025-06-30 22:17:47 -0700

    fix(srv): implement a catch-all redirect for reac-router-dom to handle any route not served by Express

--- server/index.js
+++ server/index.js
@@ -3,11 +3,23 @@
 import express from "express";
 import router from "../routes/vendors.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app
 const app = express();
 
 // middleware
 app.use(express.static("dist"));
 app.use("/api/vendors", router);
+
+// catch-all for SPA routing
+app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+});
 
 // server
 const PORT = process.env.PORT || 8080;
